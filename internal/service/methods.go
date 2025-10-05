623cecb75e9322538fa4f2aec3a719580462a8de
Ummuys <5percentofmilk@gmail.com>
2025-10-05 22:10:42 +0300

    add test for sql_inj

--- internal/service/methods.go
+++ internal/service/methods.go
@@ -2,11 +2,15 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
+	"regexp"
 	"sq/internal/cache"
 	"sq/internal/convert"
 	models "sq/internal/models/response/get"
 	"sq/internal/repository"
+	"strings"
 
 	"github.com/rs/zerolog"
 )
@@ -23,8 +27,42 @@ func NewReportService(logger *zerolog.Logger, db repository.ReportDB,
 	return &repService{logger: logger, db: db, conv: conv, chc: chc}
 }
 
+// ЗАМЕЧАНИЯ
+// 1) Сейчас функция не пропустит в любой позиции цифру 1. Надо подумать как бы это исправить, так как это не позволяет сделать, к примеру, where id = 1;
+// 2) Очень сильная блокировка не дает писать гибкие запросы. Строгую проверку, к примеру, можно убрать у людей, которые имеют токен повышенной возможности
+// 3) select * from zopa where id = 1 -- проходит, а не должна
+func checkQuery(query string) error {
+	bannedWords := []string{
+		"drop", "truncate", "delete", "update", "insert", "alter", "create",
+		"union", "into", "load_file", "pg_catalog", "information_schema",
+		"exec", "xp_cmdshell", "benchmark", "sleep", "1",
+	}
+
+	q := strings.ToLower(query)
+	if strings.Contains(query, "--") {
+		return errors.New("SQL injection / dangerous pattern detected: --")
+	}
+
+	checkList := make([]string, 0, len(bannedWords))
+	for _, w := range bannedWords {
+		checkList = append(checkList, `\b`+regexp.QuoteMeta(w)+`\b`)
+	}
+
+	reg := regexp.MustCompile("(?i)" + strings.Join(checkList, "|"))
+
+	if find := reg.FindString(q); find != "" {
+		return fmt.Errorf("SQL injection / dangerous pattern detected: %s", find)
+	}
+
+	return nil
+}
+
 func (rs *repService) CreateReport(pCtx context.Context, sql string, f *os.File) error {
 	rs.logger.Debug().Str("evt", "call CreateReport")
+
+	if err := checkQuery(sql); err != nil {
+		return err
+	}
 
 	headers, rows, err := rs.db.ExecQuery(pCtx, sql)
 	if err != nil {
@@ -34,6 +72,10 @@ func (rs *repService) CreateReport(pCtx context.Context, sql string, f *os.File)
 	err = rs.conv.ToPDF(headers, rows, f)
 	if err != nil {
 		return err
+	}
+
+	if err := rs.chc.Set(pCtx, "good key", sql); err != nil {
+		rs.logger.Error().Err(err).Str("script", sql).Msg("failed to save last query")
 	}
 
 	return nil
